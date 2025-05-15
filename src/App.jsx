ef60bc8e821cc0829e33479a4e6a5f2eae321ac1
tareq <tareqazizshihab@gmail.com>
2025-05-15 23:30:33 +0600

    demo changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Footer from "./Footer";
-import Homepage from "./Pages/Homepage";
 import Navbar from "./Navbar";
-import Shop from "./Pages/Shop";
 import ProductPage from "./Pages/ProductPage";
 
 function App() {
+  console.log("Something");
+
   return (
     <>
       <Navbar />
